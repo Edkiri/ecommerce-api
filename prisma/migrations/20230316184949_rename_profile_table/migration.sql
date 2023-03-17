b6526b5c7fcecaf3288ed29608467fe8822857ab
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-03-17 10:23:32 +0100

    prisma module created

---
+++ prisma/migrations/20230316184949_rename_profile_table/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_userId_fkey";
+
+-- DropTable
+DROP TABLE "Profile";
+
+-- CreateTable
+CREATE TABLE "profile" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "avatarUrl" TEXT,
+    "userId" UUID NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profile_userId_key" ON "profile"("userId");
+
+-- AddForeignKey
+ALTER TABLE "profile" ADD CONSTRAINT "profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
